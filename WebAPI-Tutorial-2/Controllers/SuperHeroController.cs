9a6d2a051255641caa32081ff99d73abb69c8470
David Szotek <david.szotek.vs@gmail.com>
2024-01-09 08:20:28 +0100

    Creating Model and it's controller
    Creating SuperHero model and it's Fat Controller (Fat controller meaning all the logic is in the controller, in real app I should use Services, Repositories etc. for that purpose)

---
+++ WebAPI-Tutorial-2/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Tutorial_2.Models;
+
+namespace WebAPI_Tutorial_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero> {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
---
+++ WebAPI-Tutorial-2/Models/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_Tutorial_2.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
