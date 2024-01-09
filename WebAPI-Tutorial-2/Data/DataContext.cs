5d4b723b825957e677a53dc8296c52a6c9941f0d
David Szotek <david.szotek.vs@gmail.com>
2024-01-09 08:50:32 +0100

    Creating DbContext, registering DbContext, installing NuGet packages
    -Created DataContext class for creating the DbContext (Contextualizing Model classes to Database tables
    -Registered the DbContext in Program.cs
    -Installed the neccessary NuGet Packages
         -Microsoft.EntityFrameworkCore (for creating the DbContext in DataContext.cs)
         -Microsoft.EntityFrameworkCore.SqlServer (for registering DbContext service in Program.Cs)
         -Microsoft.EntityFrameworkCore.Tools (for migrations)

---
+++ WebAPI-Tutorial-2/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Tutorial_2.Models;
+
+namespace WebAPI_Tutorial_2.Data
+{
+    // DbContext contextualizes models to a database tables
+    // 
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        // Name of the DB table, ususally just pluralised model name
+        public DbSet<SuperHero> SuperHeroes{ get; set; }
+    }
+}
--- WebAPI-Tutorial-2/Program.cs
+++ WebAPI-Tutorial-2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI_Tutorial_2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registering the DbContext service - NuGet = Microsoft.EntityFrameworkCore.SqlServer
+//For migrations - NuGet = Microsoft.EntityFrameworkCore.Tools
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
